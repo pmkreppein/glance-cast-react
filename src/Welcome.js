7a51b17710cae49e9d454cbffeb35f83597db45f
Pete Kreppein <pmkreppein@gmail.com>
2019-03-03 02:42:45 -0500

    Welcome component change

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,12 +5,20 @@ export default function Welcome() {
     <div className="box">
         <p>Welcome to Glancecast!  This is the start of a hobby project of mine to make weather info quicker to access.  This is also a portfolio project for the Flatiron Online Code Bootcamp
         that I am working on.  Right now, this websites includes <a href="https://darksky.net/">Darksky</a> data and <a href="https://www.weather.gov/forecastmaps">National Weather Service</a> 
-        forecast maps.  Darksky offers a great API and the National Weather Service has troves of data all available for free!
-
+         forecast maps.  Darksky offers a great API and the National Weather Service has troves of data all available for free!
+        </p>
+        <p>
         I intend to continue building this project out, so check back soon as updates are posted!  You can learn more about me <a href="https://pmkdev.com">here.</a>  Thanks for visiting!
-
+        </p>
+        <p>
         Below is a visual representation of the National Weather Service's daily nationwide forecasts.
         </p>
+        <p><strong>Instructions for use:</strong></p>
+        <p>For three days of forecast maps like the one below, click <a href="/nation-overview">NWS Nation Overview.</a>  Select the city from the City Forecasts dropdown for detailed weather.  Toggle
+        between Farenheit and Celsius by clicking on the "Metric Mode" button.
+        </p>
+
+        
     </div>
 
   )
